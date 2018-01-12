06dfd6dda10e309673256bfc82af8220b640044f
maldevel <maldevel@mail.com>
2018-01-12 11:37:23 +0200

    dns tld expansion added

--- gasmask.py
+++ gasmask.py
@@ -41,6 +41,7 @@ import socket
 import whois
 import dns.resolver
 import collections
+import re
 
 #######################################################
 
@@ -127,63 +128,120 @@ def dnsQuery(value):
         try:
             answers = dns.resolver.query(value, rec)
             for answer in answers:
-                dnsData[rec].append(answer.to_text())
+            	if rec is 'NS':
+            		dnsData[rec].append(answer.to_text() + ":" + verifyHostname(answer.to_text()))
+            	elif rec is 'MX':
+            		domain_name = re.search(' (.*)\.', answer.to_text(), re.IGNORECASE).group(1)
+            		dnsData[rec].append(answer.to_text() + ":" + verifyHostname(domain_name))
+            	else:
+                	dnsData[rec].append(answer.to_text())
         except Exception as e:
-            dnsData[rec].append('-')
+			dnsData[rec].append('-')
 
     return dnsData
+
+###
+
+def tldQuery(value):
+	tldData = []
+
+	tlds = [
+            "ac", "academy", "ad", "ae", "aero", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "arpa", "as",
+            "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bike", "biz", "bj",
+            "bm", "bn", "bo", "br", "bs", "bt", "builders", "buzz", "bv", "bw", "by", "bz", "ca", "cab", "camera",
+            "camp", "careers", "cat", "cc", "cd", "center", "ceo", "cf", "cg", "ch", "ci", "ck", "cl", "clothing",
+            "cm", "cn", "co", "codes", "coffee", "com", "company", "computer", "construction", "contractors", "coop",
+            "cr", "cu", "cv", "cw", "cx", "cy", "cz", "de", "diamonds", "directory", "dj", "dk", "dm", "do",
+            "domains", "dz", "ec", "edu", "education", "ee", "eg", "email", "enterprises", "equipment", "er", "es",
+            "estate", "et", "eu", "farm", "fi", "fj", "fk", "florist", "fm", "fo", "fr", "ga", "gallery", "gb", "gd",
+            "ge", "gf", "gg", "gh", "gi", "gl", "glass", "gm", "gn", "gov", "gp", "gq", "gr", "graphics", "gs", "gt",
+            "gu", "guru", "gw", "gy", "hk", "hm", "hn", "holdings", "holiday", "house", "hr", "ht", "hu", "id", "ie",
+            "il", "im", "immobilien", "in", "info", "institute", "int", "international", "io", "iq", "ir", "is", "it",
+            "je", "jm", "jo", "jobs", "jp", "kaufen", "ke", "kg", "kh", "ki", "kitchen", "kiwi", "km", "kn", "kp",
+            "kr", "kw", "ky", "kz", "la", "land", "lb", "lc", "li", "lighting", "limo", "lk", "lr", "ls", "lt", "lu",
+            "lv", "ly", "ma", "management", "mc", "md", "me", "menu", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo",
+            "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "museum", "mv", "mw", "mx", "my", "mz", "na", "name", "nc",
+            "ne", "net", "nf", "ng", "ni", "ninja", "nl", "no", "np", "nr", "nu", "nz", "om", "onl", "org", "pa", "pe",
+            "pf", "pg", "ph", "photography", "photos", "pk", "pl", "plumbing", "pm", "pn", "post", "pr", "pro", "ps",
+            "pt", "pw", "py", "qa", "re", "recipes", "repair", "ro", "rs", "ru", "ruhr", "rw", "sa", "sb", "sc", "sd",
+            "se", "sexy", "sg", "sh", "shoes", "si", "singles", "sj", "sk", "sl", "sm", "sn", "so", "solar",
+            "solutions", "sr", "st", "su", "support", "sv", "sx", "sy", "systems", "sz", "tattoo", "tc", "td",
+            "technology", "tel", "tf", "tg", "th", "tips", "tj", "tk", "tl", "tm", "tn", "to", "today", "tp", "tr",
+            "training", "travel", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "uno", "us", "uy", "uz", "va", "vc",
+            "ve", "ventures", "vg", "vi", "viajes", "vn", "voyage", "vu", "wang", "wf", "wien", "ws", "xxx", "ye",
+            "yt", "za", "zm", "zw"]
+
+	for tld in tlds:
+		try:
+			hostname = value.split('.')[0] + '.' + tld
+			answers = dns.resolver.query(hostname, 'A')
+			for answer in answers:
+				tldData.append(hostname + ":" + answer.to_text())
+		except Exception as e:
+			pass
+
+	return tldData
 
 #######################################################
 
 if __name__ == '__main__':
-    print message
-    info = {}
+	print message
+	info = {}
 
-    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
-    parser.add_argument("-d", '--domain', action="store", metavar='DOMAIN', dest='domain', 
+	parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+	parser.add_argument("-d", '--domain', action="store", metavar='DOMAIN', dest='domain', 
                         default=None, type=checkDomain, help="Domain to search.")
     #parser.add_argument('-o', '--output', action='store', metavar='BASENAME', dest='basename', 
-    #                    type=str, default=None, help='Output in the three major formats at once')
+    #                    type=str, default=None, help='Output in the four major formats at once.')
 
-    if len(sys.argv) is 1:
-        parser.print_help()
-        sys.exit()
+	if len(sys.argv) is 1:
+		parser.print_help()
+		sys.exit()
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    info['domain'] = args.domain
-
-#######################################################
-
-    print "[+] Target:"
-    print "-----------"
-    info['ip'] = verifyHostname(info['domain'])
-    print info['domain'] + ":" + info['ip'] + "\n"
+	info['domain'] = args.domain
 
 #######################################################
 
-    print "[+] Whois:"
-    print "----------"
-    info['whois'] = whoisQuery(info['domain'])
-    for key,value in info['whois'].iteritems():
-        if isinstance(value[0], list):
-            print
-            print value[1]
-            for val in value[0]:
-                print val
-            print
-        else:
-            print value[1] + " " + value[0]
+	print "[+] Target:"
+	print "-----------"
+	info['ip'] = verifyHostname(info['domain'])
+	print info['domain'] + ":" + info['ip'] + "\n"
 
 #######################################################
 
-    print "[+] DNS:"
-    print "--------"
-    info['dns'] = dnsQuery(info['domain'])
-    for key,value in info['dns'].iteritems():
-        print key + " DNS record: "
-        for val in value:
-            print val
-        print
+	print "[+] Whois:"
+	print "----------"
+	info['whois'] = whoisQuery(info['domain'])
+	for key,value in info['whois'].iteritems():
+		if isinstance(value[0], list):
+			print
+			print value[1]
+			for val in value[0]:
+				print val
+			print
+		else:
+			print value[1] + " " + value[0]
+
+#######################################################
+
+	print "[+] DNS:"
+	print "--------"
+	info['dns'] = dnsQuery(info['domain'])
+	for key,value in info['dns'].iteritems():
+		print key + " DNS record: "
+		for val in value:
+			print val
+		print
+
+#######################################################
+
+	print "[+] DNS TLD expansion:"
+	print "--------"
+	info['tld'] = tldQuery(info['domain'])
+	for val in info['tld']:
+		print val
+	print
 
 #######################################################
