22a66a8ed721beca4131943c217a7ea73b3f170b
maldevel <maldevel@mail.com>
2018-01-21 20:17:01 +0200

    readme updated

--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 
 All in one Information gathering tool - OSINT
 
+### Information Gathering
+
+* ask
+* bing
+* crt
+* dns
+* dogpile
+* github
+* google
+* googleplus
+* instagram
+* linkedin
+* netcraft
+* pgp
+* reddit
+* reverse dns
+* twitter
+* vhosts
+* virustotal
+* whois
+* yahoo
+* yandex
+* youtube
+
 ### Credits
 
 * [EmailHarvester](https://github.com/maldevel/EmailHarvester)
--- gasmask.py
+++ gasmask.py
@@ -614,7 +614,7 @@ def MainFunc():
 	timeouts = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 	modes = ['all', 'whois', 'dns', 'revdns', 'vhosts', 'google', 'bing', 'yahoo',
 		'ask', 'dogpile', 'yandex', 'linkedin', 'twitter', 'googleplus', 'youtube', 'reddit',
-		'github', 'instagram', 'crt', 'pgp', 'netcraft']
+		'github', 'instagram', 'crt', 'pgp', 'netcraft', 'virustotal']
 
 	parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
 
